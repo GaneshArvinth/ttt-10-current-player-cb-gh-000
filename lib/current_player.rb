e7f10b5969b8668d513fbded9283377b5aadc295
Ganesh Arvinth <ganesharvinth0@gmail.com>
2018-04-20 07:22:40 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   count = 0
   board.each do |elements|
-    if (elements == "X" || elements == "Y")
+    if (elements == "X" || elements == "O")
       count+=1
     end
   end
